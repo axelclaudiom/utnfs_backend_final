2ab5c7a8238405cf2de2ac8ecc171171df443919
Axel Claudio Melgar <axelclaudiom@gmail.com>
2024-12-09 12:49:34 -0300

    add: Route auth

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 
+const authRoutes = require('./routes/authRoutes');
+const articuloRoutes = require('./routes/articuloRoutes');
+
 const app = express();
 connectDB();
 
 app.get('/', (req, res) => {
   res.send('API en construcción');
 });
+
+// Rutas
+app.use('/api/auth', authRoutes);
+app.use('/api/articulos', articuloRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
---
+++ routes/authRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).json({ message: 'Usuario registrado correctamente' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: 'Credenciales inválidas' });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Error en el servidor' });
+  }
+});
+
+module.exports = router;
