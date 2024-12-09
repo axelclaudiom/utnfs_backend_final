3ad28a061776a37e4d32979c47f3fe1eed72e393
Axel Claudio Melgar <axelclaudiom@gmail.com>
2024-12-09 10:54:04 -0300

    config conection to db

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API en construcción');
