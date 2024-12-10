b337be2260452d358dcd31c18d39ff06eff0b895
Axel Claudio Melgar <axelclaudiom@gmail.com>
2024-12-09 23:18:15 -0300

    helmet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const helmet = require('helmet');
 
 const authRoutes = require('./routes/authRoutes');
 const articuloRoutes = require('./routes/articuloRoutes');
@@ -8,11 +9,14 @@ const articuloRoutes = require('./routes/articuloRoutes');
 const app = express();
 connectDB();
 
+// Middlewares
+app.use(helmet());
 app.use(express.json());
 
 // Rutas
 app.use('/api/auth', authRoutes);
 app.use('/api/articulos', articuloRoutes);
 
+// Servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
